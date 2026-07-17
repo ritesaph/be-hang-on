05c71d71ea5b18fa65e3b7f527e221690374bded
ritesaph <164435387+ritesaph@users.noreply.github.com>
2026-07-17 11:17:21 +0700

    fix: session.py

--- app/db/session.py
+++ app/db/session.py
@@ -7,6 +7,8 @@ from app.config import settings
 engine = create_async_engine(
     settings.database_url,
     connect_args={"ssl": "require", "statement_cache_size": 0},
+    pool_pre_ping=True,
+    pool_recycle=300,
 )
 
 async_session_factory = async_sessionmaker(engine, expire_on_commit=False)
